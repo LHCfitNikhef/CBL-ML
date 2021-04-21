23d1582d687fcf549895e9894d20e90ae1a0376a
Abel B <a.r.brokkelkamp@tudelft.nl>
2021-04-21 16:15:01 +0200

    slight changes to plot_results_dE.py

--- EELS_KK/pyfiles/bash_train_pyfiles/plot_results_dE.py
+++ EELS_KK/pyfiles/bash_train_pyfiles/plot_results_dE.py
@@ -35,88 +35,21 @@ def gen_ZLP_I(image, I):
 im = Spectral_image.load_data('E:/PhD/data/m20210331/eels/eels-SI/10n-dop-inse-B1_stem-eels-SI-processed_003.dm4')
 # im.cluster(5)
 
-#im=im
-
-<<<<<<< Updated upstream
-# path_to_models = 'dE1/E1_05'
-# path_to_models = 'models/train_lau_log'
-path_to_models = 'models/train_004_pooled_5_3'
-# path_to_models = 'models/train_004'
-=======
 path_to_models = 'E:/PhD/data/MLdata/models/dE_n10-inse_SI-003/E1_05'
-
->>>>>>> Stashed changes
  
 im.load_ZLP_models_smefit(path_to_models=path_to_models)
-xlim = [np.min(im.dE1[1,:])/4, np.max(im.dE1[1,:])*1.5]
+xlim = [np.min(im.dE1[1,:])/4, np.max(im.dE1[1,:])*2.5]
 
-name = " 004"
+name = "10n-InSe SI-003"
 
+"""
+# Predictions for scaled intensities, plot 1 complete spectrum, plot 2 zoomed in to [0, xlim] eV
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
 ax1.set_title("predictions for scaled intensities 0.1-0.9 of " + name)
 ax1.set_xlabel("energy loss [eV]")
 ax1.set_ylabel("intensity")
 ax2.set_title("predictions for scaled intensities 0.1-0.9 of " + name)
-ax2.set_xlabel("energy loss [eV]")
-ax2.set_ylabel("intensity")
-
-
-
-
-for I in [0.2,0.3,0.5,0.7,0.8]:
-    ZLPs = gen_ZLP_I(im, I)
-    low = np.nanpercentile(ZLPs, 16, axis=0)
-    high = np.nanpercentile(ZLPs, 84, axis=0)
-    mean = np.average(ZLPs, axis = 0)
-    mean = np.nanpercentile(ZLPs, 50, axis=0)
-    #[mean, var, low, high], edges = binned_statistics(im.deltaE, ZLPs, n_bins, ["mean", "var", "low", "high"])
-    ax1.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax2.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax1.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    ax2.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-
-
-    
-ax2.set_ylim(-100,1e3)
-ax2.set_xlim(xlim)# 0,2)
-ax1.legend()
-ax2.legend()
-
-#%%
-
-fig5, ax5 = plt.subplots()
-fig6, ax6 = plt.subplots()
-ax5.set_title("predictions for spectrum pixxel[50,60] of " + name)
-ax5.set_xlabel("energy loss [eV]")
-ax5.set_ylabel("intensity")
-ax6.set_title("predictions for spectrum pixxel[50,60] of " + name)
-ax6.set_xlabel("energy loss [eV]")
-ax6.set_ylabel("intensity")
-ax6.set_ylim(-200,1.5e3)
-ax6.set_xlim(xlim)
-
-ZLPs = im.calc_ZLPs(20,20)
-low = np.nanpercentile(ZLPs, 16, axis=0)
-high = np.nanpercentile(ZLPs, 84, axis=0)
-mean = np.average(ZLPs, axis = 0)
-mean = np.nanpercentile(ZLPs, 50, axis=0)
-ax5.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax6.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax5.plot(im.deltaE, mean, label = "ddE = 0")
-ax6.plot(im.deltaE, mean, label = "ddE = 0")
-
-
-
-
-"""
-#
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-ax1.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = 0")
-ax1.set_xlabel("energy loss [eV]")
-ax1.set_ylabel("intensity")
-ax2.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = 0")
 ax2.set_xlabel("energy loss [eV]")
 ax2.set_ylabel("intensity")
 
@@ -129,20 +62,7 @@ ax4.set_title("predictions for scaled intensity 0.5 at ddE1 = 0, -0.3 and -0.5")
 ax4.set_xlabel("energy loss [eV]")
 ax4.set_ylabel("intensity")
 
-
-fig5, ax5 = plt.subplots()
-fig6, ax6 = plt.subplots()
-ax5.set_title("predictions for spectrum pixxel[50,60] at ddE1 = 0, -0.3 and -0.5")
-ax5.set_xlabel("energy loss [eV]")
-ax5.set_ylabel("intensity")
-ax6.set_title("predictions for spectrum pixxel[50,60] at ddE1 = 0, -0.3 and -0.5")
-ax6.set_xlabel("energy loss [eV]")
-ax6.set_ylabel("intensity")
-ax6.set_ylim(-500,2e3)
-ax6.set_xlim(0,2)
-
-
-for I in [0.1,0.3,0.5,0.7,0.9]:
+for I in [0.2,0.3,0.5,0.7,0.8]:
     ZLPs = gen_ZLP_I(im, I)
     low = np.nanpercentile(ZLPs, 16, axis=0)
     high = np.nanpercentile(ZLPs, 84, axis=0)
@@ -153,22 +73,35 @@ for I in [0.1,0.3,0.5,0.7,0.9]:
     ax2.fill_between(im.deltaE, low, high, alpha = 0.3)
     ax1.plot(im.deltaE, mean, label = "I_scales = " + str(I))
     ax2.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    
     if I == 0.5:
         ax3.fill_between(im.deltaE, low, high, alpha = 0.2)
         ax4.fill_between(im.deltaE, low, high, alpha = 0.2)
         ax3.plot(im.deltaE, mean, label = "ddE = 0")
         ax4.plot(im.deltaE, mean, label = "ddE = 0")
-ax2.set_ylim(-500,6e3)
-ax2.set_xlim(0,2)
+ 
+ax2.set_ylim(-100,1e3)
+ax2.set_xlim(xlim)
 ax1.legend()
 ax2.legend()
 ax4.set_ylim(-500,6e3)
-ax4.set_xlim(0,2)
+ax4.set_xlim(xlim)
 ax3.legend()
 ax4.legend()
 
-ZLPs = im.calc_ZLPs(50,60)
+
+
+fig5, ax5 = plt.subplots()
+fig6, ax6 = plt.subplots()
+ax5.set_title("predictions for spectrum pixel[20,20] of " + name)
+ax5.set_xlabel("energy loss [eV]")
+ax5.set_ylabel("intensity")
+ax6.set_title("predictions for spectrum pixel[20,20] of " + name)
+ax6.set_xlabel("energy loss [eV]")
+ax6.set_ylabel("intensity")
+ax6.set_ylim(-200,1.5e3)
+ax6.set_xlim(xlim)
+
+ZLPs = im.calc_ZLPs(20,20)
 low = np.nanpercentile(ZLPs, 16, axis=0)
 high = np.nanpercentile(ZLPs, 84, axis=0)
 mean = np.average(ZLPs, axis = 0)
@@ -177,153 +110,53 @@ ax5.fill_between(im.deltaE, low, high, alpha = 0.2)
 ax6.fill_between(im.deltaE, low, high, alpha = 0.2)
 ax5.plot(im.deltaE, mean, label = "ddE = 0")
 ax6.plot(im.deltaE, mean, label = "ddE = 0")
-
-
-
-
-
-path_to_models = 'dE1/E1_03'
-im.load_ZLP_models_smefit(n_rep = 500, path_to_models=path_to_models)
-
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-ax1.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = -0.3")
-ax1.set_xlabel("energy loss [eV]")
-ax1.set_ylabel("intensity")
-ax2.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = -0.3")
-ax2.set_xlabel("energy loss [eV]")
-ax2.set_ylabel("intensity")
-
-
-
-
-for I in [0.1,0.3,0.5,0.7,0.9]:
-    ZLPs = gen_ZLP_I(im, I)
-    low = np.nanpercentile(ZLPs, 16, axis=0)
-    high = np.nanpercentile(ZLPs, 84, axis=0)
-    mean = np.average(ZLPs, axis = 0)
-    mean = np.nanpercentile(ZLPs, 50, axis=0)
-    #[mean, var, low, high], edges = binned_statistics(im.deltaE, ZLPs, n_bins, ["mean", "var", "low", "high"])
-    ax1.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax2.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax1.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    ax2.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    if I == 0.5:
-        ax3.fill_between(im.deltaE, low, high, alpha = 0.2)
-        ax4.fill_between(im.deltaE, low, high, alpha = 0.2)
-        ax3.plot(im.deltaE, mean, label = "ddE = -0.3")
-        ax4.plot(im.deltaE, mean, label = "ddE = -0.3")
-ax2.set_ylim(-500,6e3)
-ax2.set_xlim(0,2)
-ax1.legend()
-ax2.legend()
-    
-
-ZLPs = im.calc_ZLPs(50,60)
-low = np.nanpercentile(ZLPs, 16, axis=0)
-high = np.nanpercentile(ZLPs, 84, axis=0)
-mean = np.average(ZLPs, axis = 0)
-mean = np.nanpercentile(ZLPs, 50, axis=0)
-ax5.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax6.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax5.plot(im.deltaE, mean, label = "ddE = -0.3")
-ax6.plot(im.deltaE, mean, label = "ddE = -0.3")
-
-
-path_to_models = 'dE1/E1_05'
-im.load_ZLP_models_smefit(n_rep = 500, path_to_models=path_to_models)
-
-fig1, ax1 = plt.subplots()
-fig2, ax2 = plt.subplots()
-ax1.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = -0.5")
-ax1.set_xlabel("energy loss [eV]")
-ax1.set_ylabel("intensity")
-ax2.set_title("predictions for scaled intensities 0.1-0.9 at ddE1 = -0.5")
-ax2.set_xlabel("energy loss [eV]")
-ax2.set_ylabel("intensity")
-
-
-
-
-for I in [0.1,0.3,0.5,0.7,0.9]:
-    ZLPs = gen_ZLP_I(im, I)
-    low = np.nanpercentile(ZLPs, 16, axis=0)
-    high = np.nanpercentile(ZLPs, 84, axis=0)
-    mean = np.average(ZLPs, axis = 0)
-    mean = np.nanpercentile(ZLPs, 50, axis=0)
-    #[mean, var, low, high], edges = binned_statistics(im.deltaE, ZLPs, n_bins, ["mean", "var", "low", "high"])
-    ax1.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax2.fill_between(im.deltaE, low, high, alpha = 0.3)
-    ax1.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    ax2.plot(im.deltaE, mean, label = "I_scales = " + str(I))
-    if I == 0.5:
-        ax3.fill_between(im.deltaE, low, high, alpha = 0.2)
-        ax4.fill_between(im.deltaE, low, high, alpha = 0.2)
-        ax3.plot(im.deltaE, mean, label = "ddE = -0.5")
-        ax4.plot(im.deltaE, mean, label = "ddE = -0.5")
-ax2.set_ylim(-500,6e3)
-ax2.set_xlim(0,2)
-ax1.legend()
-ax2.legend()
-ax3.legend()
-ax4.legend()
 """
-# path_to_models = 'dE1/E1_05'
-# im.load_ZLP_models_smefit(path_to_models=path_to_models)
 
-
-fig5, ax5 = plt.subplots()
-fig6, ax6 = plt.subplots()
-ax5.set_title("ZLP matching results at pixel[50,60]" + name)
-ax5.set_xlabel("energy loss [eV]")
-ax5.set_ylabel("intensity")
-ax6.set_title("ZLP matching results at pixel[50,60]" + name)
-ax6.set_xlabel("energy loss [eV]")
-ax6.set_ylabel("intensity")
-ax6.set_ylim(-500,3e3)
-ax6.set_ylim(-200,1.5e3)
-ax6.set_xlim(xlim)
-
-
-ZLPs = im.calc_gen_ZLPs(20,20)
-low = np.nanpercentile(ZLPs, 16, axis=0)
-high = np.nanpercentile(ZLPs, 84, axis=0)
-mean = np.average(ZLPs, axis = 0)
-mean = np.nanpercentile(ZLPs, 50, axis=0)
-ax5.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax6.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax5.plot(im.deltaE, mean, label = "gen")
-ax6.plot(im.deltaE, mean, label = "gen")
-
-signal = im.get_pixel_signal(20,20)
-#ax5.plot(im.deltaE, signal, label = "signal")
-#ax6.plot(im.deltaE, signal, label = "signal")
-#"""
-ZLPs = im.calc_ZLPs(20,20)
-low = np.nanpercentile(ZLPs, 16, axis=0)
-high = np.nanpercentile(ZLPs, 84, axis=0)
-mean = np.average(ZLPs, axis = 0)
-mean = np.nanpercentile(ZLPs, 50, axis=0)
-ax5.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax6.fill_between(im.deltaE, low, high, alpha = 0.2)
-ax5.plot(im.deltaE, mean, label = "matched")
-ax6.plot(im.deltaE, mean, label = "matched")
-
-ZLPs = im.calc_ZLPs(20,20)
-low = np.nanpercentile(ZLPs, 16, axis=0)
-high = np.nanpercentile(ZLPs, 84, axis=0)
-mean = np.average(ZLPs, axis = 0)
-mean = np.nanpercentile(ZLPs, 50, axis=0)
-ax5.fill_between(im.deltaE, signal-low, signal-high, alpha = 0.2)
-ax6.fill_between(im.deltaE, signal-low, signal-high, alpha = 0.2)
-ax5.plot(im.deltaE, signal-mean, label = "result")
-ax6.plot(im.deltaE, signal-mean, label = "result")
-
-
-#"""
-ax5.legend()
-ax6.legend()
-
-
-
-
+# Plot single spectrum of pixel
+for i in range(1,7):
+    fig7, ax7 = plt.subplots()
+    fig8, ax8 = plt.subplots()
+    ax7.set_title("ZLP matching results at pixel[30," + str(20*i) + "]" + name)
+    ax7.set_xlabel("energy loss [eV]")
+    ax7.set_ylabel("intensity")
+    ax8.set_title("ZLP matching results at pixel[30," + str(20*i) + "]" + name)
+    ax8.set_xlabel("energy loss [eV]")
+    ax8.set_ylabel("intensity")
+    ax8.set_ylim(-500,3e3)
+    ax8.set_ylim(-200,1.5e3)
+    ax8.set_xlim(xlim)
+    
+    signal = im.get_pixel_signal(30,20*i)
+    
+    ZLPs = im.calc_gen_ZLPs(30,20*i)
+    low = np.nanpercentile(ZLPs, 16, axis=0)
+    high = np.nanpercentile(ZLPs, 84, axis=0)
+    mean = np.average(ZLPs, axis = 0)
+    mean = np.nanpercentile(ZLPs, 50, axis=0)
+    ax7.fill_between(im.deltaE, low, high, alpha = 0.2)
+    ax8.fill_between(im.deltaE, low, high, alpha = 0.2)
+    ax7.plot(im.deltaE, mean, label = "gen")
+    ax8.plot(im.deltaE, mean, label = "gen")
+    
+    ZLPs = im.calc_ZLPs(30,20*i)
+    low = np.nanpercentile(ZLPs, 16, axis=0)
+    high = np.nanpercentile(ZLPs, 84, axis=0)
+    mean = np.average(ZLPs, axis = 0)
+    mean = np.nanpercentile(ZLPs, 50, axis=0)
+    ax7.fill_between(im.deltaE, low, high, alpha = 0.2)
+    ax8.fill_between(im.deltaE, low, high, alpha = 0.2)
+    ax7.plot(im.deltaE, mean, label = "matched")
+    ax8.plot(im.deltaE, mean, label = "matched")
+    
+    ZLPs = im.calc_ZLPs(30,20*i)
+    low = np.nanpercentile(ZLPs, 16, axis=0)
+    high = np.nanpercentile(ZLPs, 84, axis=0)
+    mean = np.average(ZLPs, axis = 0)
+    mean = np.nanpercentile(ZLPs, 50, axis=0)
+    ax7.fill_between(im.deltaE, signal-low, signal-high, alpha = 0.2)
+    ax8.fill_between(im.deltaE, signal-low, signal-high, alpha = 0.2)
+    ax7.plot(im.deltaE, signal-mean, label = "result")
+    ax8.plot(im.deltaE, signal-mean, label = "result")
+    
+    ax7.legend()
+    ax8.legend()
