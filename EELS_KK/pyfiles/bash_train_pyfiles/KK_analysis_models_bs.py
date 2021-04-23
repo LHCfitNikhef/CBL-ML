5d511bf3146f9540b05137603a7b3d4895b8cc81
Jaco ter Hoeve <j.j.ter.hoeve@vu.nl>
2021-04-23 15:28:05 +0200

    sphinx website framework

--- EELS_KK/pyfiles/bash_train_pyfiles/KK_analysis_models_bs.py
+++ EELS_KK/pyfiles/bash_train_pyfiles/KK_analysis_models_bs.py
@@ -11,32 +11,33 @@ import sys
 from image_class_bs import Spectral_image
 import seaborn as sns
 
+if __name__ == '__main__':
 
 
-bs_rep_num = int(sys.argv[1])
-path_to_models = sys.argv[2]
-path_to_save = sys.argv[3]
+    bs_rep_num = int(sys.argv[1])
+    path_to_models = sys.argv[2]
+    path_to_save = sys.argv[3]
 
-try:
-    smooth = bool(int(sys.argv[4]))
-except:
-    smooth = False  
+    try:
+        smooth = bool(int(sys.argv[4]))
+    except:
+        smooth = False
 
 
-# bs_rep_num = 1
-# path_to_models = "./dE1/E1_05"
-# path_to_save = "./dE1"
-# im = Spectral_image.load_data('../../dmfiles/h-ws2_eels-SI_004.dm4')
+    # bs_rep_num = 1
+    # path_to_models = "./dE1/E1_05"
+    # path_to_save = "./dE1"
+    # im = Spectral_image.load_data('../../dmfiles/h-ws2_eels-SI_004.dm4')
 
-im = Spectral_image.load_data('/data/theorie/ipostmes/cluster_programs/EELS_KK/dmfiles/h-ws2_eels-SI_004.dm4')
+    im = Spectral_image.load_data('/data/theorie/ipostmes/cluster_programs/EELS_KK/dmfiles/h-ws2_eels-SI_004.dm4')
 
-if not im.check_cost_smefit(path_to_models, bs_rep_num):
-    sys.exit()
+    if not im.check_cost_smefit(path_to_models, bs_rep_num):
+        sys.exit()
 
-im.cluster(5)
-im.load_ZLP_models_smefit(path_to_models, idx = bs_rep_num)   
+    im.cluster(5)
+    im.load_ZLP_models_smefit(path_to_models, idx = bs_rep_num)
 
-im.im_dielectric_function_bs(save_index = bs_rep_num, save_path = path_to_save)
+    im.im_dielectric_function_bs(save_index = bs_rep_num, save_path = path_to_save)
 
 
 
--- sphinx/build/html/_sources/modules/modules.rst.txt
+++ sphinx/build/html/_sources/modules/modules.rst.txt
@@ -1,21 +1,10 @@
-bash_train_pyfiles
-==================
+API
+===
 
-.. toctree::
-   :maxdepth: 4
+.. autosummary::
+   :toctree: _autosummary
+   :template: module.rst
 
-   KK_MPI_per_row
-   KK_analysis_models_bs
-   KK_meta
-   KK_one_pixel
-   KK_per_row
-   KK_pixel
-   KK_plot_results
-   KK_save_results
-   calculate_dist_KK_results
    image_class_bs
    k_means_clustering
-   plot_results_dE
    train_nn_torch_bs
-   training_images_bs
-   untitled7
