baded40a0346c00b9b75f8f0f9bf81a67ffdf857
jacoterh <54140851+jacoterh@users.noreply.github.com>
2021-07-19 10:11:41 +0200

    Adding option to choose the replica method or the local minimum method to find dE1. Also adding some chi2 plots.

--- EELS_KK/code/training_init.py
+++ EELS_KK/code/training_init.py
@@ -14,19 +14,20 @@ n_epochs = 10000  # number of epochs
 display_step = 10
 
 im = SpectralImage.load_data(dm4_path)
-im.cluster(5)
-im.pool(5)
+#im.cluster(5)
+#im.pool(5)
 path_to_models = '/data/theorie/abelbk/bash_train_pyfiles/models/dE_nf-ws2_SI-001/E1_new/'
-im.calc_gen_ZLPs(30, 30, signal="pooled", path_to_models=path_to_models)
+#im.calc_gen_ZLPs(30, 30, signal="pooled", path_to_models=path_to_models)
 
 
-im.load_ZLP_models_smefit(path_to_models=path_to_models)
+#im.load_ZLP_models_smefit(path_to_models=path_to_models)
 
 im.train_zlp(n_clusters=n_clusters,
              n_rep=n_rep,
              n_epochs=n_epochs,
              bs_rep_num=bs_rep_num,
              path_to_models=path_to_models,
-             display_step=display_step)
+             display_step=display_step,
+             plotde1=False)
 
 # TODO: kwargs dE1 option
